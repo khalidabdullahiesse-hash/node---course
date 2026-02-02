050c0c4188b5b99210a44c085a978e5d3845080a
KHALID ABDULLAHI ISSE <khalidabdulahiesse@gmail.com>
2026-02-02 12:05:48 +0300

    first commit

---
+++ node-app/app.js
@@ -0,0 +1,4 @@
+const utils = require('./utils.js');
+
+console.log('Khalid abdullahi Isse');
+
---
+++ node-app/utils.js
@@ -0,0 +1,9 @@
+console.log('NODE APP')
+
+
+
+const getName = function (name){
+    console(name + 'Welcome to khalid node Workstation')
+}
+
+module.export = getName;
