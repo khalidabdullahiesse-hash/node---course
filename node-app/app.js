08947b6d84f59d896738de5b345d6798c9167a89
KHALID ABDULLAHI ISSE <khalidabdulahiesse@gmail.com>
2026-02-04 06:50:10 +0300

    adding more moudles

--- node-app/app.js
+++ node-app/app.js
@@ -1,8 +1,11 @@
 
 
 import * as utils from './utils.js'
-
 import validator from 'validator'
+import chalk from 'chalk';
 
 
-console.log('Khalid abdullahi Isse');
+
+
+console.log(chalk.blue.bgCyan.bold(utils.getUserEmail('khalidabdullahiIssa@gmail.com')));
+console.log(chalk.blue.bgCyan.bold(utils.getUserURL('http://khalid.com')));
--- node-app/utils.js
+++ node-app/utils.js
@@ -1,9 +1,21 @@
-console.log('NODE APP')
+import validator from 'validator'
+import chalk from 'chalk'
+
+console.log(chalk.bgCyan('NODE APP'));
 
 
 
 const getName = function (name){
-    console(name + 'Welcome to khalid node Workstation')
+    console.log(name + 'Welcome to khalid node Workstation')
 }
 
-export {getName}
+const getUserEmail = function(email){
+
+    const userValidation = validator.isEmail(email);
+    console.log(chalk.red.bold(userValidation));
+}
+const getUserURL = function(url){
+    const userValidation = validator.isURL(url);
+    console.log(chalk.red.italic(userValidation));
+}
+export {getName , getUserEmail, getUserURL};
