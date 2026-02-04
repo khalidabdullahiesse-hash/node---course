36b442fb13b17f46a7f29ce1f4712391b925752a
KHALID ABDULLAHI ISSE <khalidabdulahiesse@gmail.com>
2026-02-04 05:27:44 +0300

    adding the new files and modules

--- node-app/app.js
+++ node-app/app.js
@@ -1,4 +1,8 @@
-const utils = require('./utils.js');
+
+
+import * as utils from './utils.js'
+
+import validator from 'validator'
+
 
 console.log('Khalid abdullahi Isse');
-
--- node-app/utils.js
+++ node-app/utils.js
@@ -6,4 +6,4 @@ const getName = function (name){
     console(name + 'Welcome to khalid node Workstation')
 }
 
-module.export = getName;
+export {getName}
