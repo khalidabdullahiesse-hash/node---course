8a21444f647e831a006e41658535c6af5eb007f3
KHALID ABDULLAHI ISSE <khalidabdulahiesse@gmail.com>
2026-02-05 22:03:15 +0300

    adding code

--- node-app/app.js
+++ node-app/app.js
@@ -3,6 +3,13 @@
 import * as utils from './utils.js'
 import validator from 'validator'
 import chalk from 'chalk';
+import isEmail from 'validator/lib/isEmail.js';
+import isURL from 'validator/lib/isURL.js';
+
+console.log(chalk.bold.red(process.argv));
 
 
-console.log(process.argv)
+const email = isEmail("khalidabdullahiisse@gmail.com");
+const url = isURL("http://khalid.com")
+
+console.log(url, email)
