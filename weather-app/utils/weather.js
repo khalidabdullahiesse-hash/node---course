7a5d8d5caaf3fe019ccfffa720752ae50d0ace9d
KHALID ABDULLAHI ISSE <khalidabdulahiesse@gmail.com>
2026-02-22 06:17:21 +0300

    Removed secret and added env protection

--- weather-app/utils/weather.js
+++ weather-app/utils/weather.js
@@ -1,21 +1,27 @@
 import request from "postman-request";
 
+const weather = (latitude, longitude, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=0782296248832d7408e063b4f567ed2c&query=${encodeURIComponent(latitude)},${encodeURIComponent(longitude)}`;
 
-const weather= (latitude, longitude, callback) => {
-  const url =   `http://api.weatherstack.com/current?access_key=0782296248832d7408e063b4f567ed2c&query=${encodeURIComponent(latitude)},${encodeURIComponent(longitude)}`;
   request({ url: url, json: true }, (error, response) => {
-    if(error){
-      callback('There is not Network services',undefined);
-    } else if(response.body.error){
-      callback('your search has been dined', undefined);
-    } else{
-      callback(undefined,{
-        temperature: response.body.current.temperature,
-        weather_description: response.body.current.weather_descriptions[0]
+
+    if (error) {
+      callback("There is no Network services", undefined);
+
+    } else if (response.body.error) {
+      callback("Your search has been denied", undefined);
+
+    } else {
+      const temperature = response.body.current.temperature;
+      const weather_description = response.body.current.weather_descriptions[0];
+
+      callback(undefined, {
+        temperature,
+        weather_description
       });
     }
+
   });
 };
 
-
-export default weather
+export default weather;
